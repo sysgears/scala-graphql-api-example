984a23f9fbac47b67d8535dc3f7c91beb5d7ce92
pavel.grudina <plgrudina@gmail.com>
2019-01-03 18:05:14 +0200

    Added resolver implementation

--- app/resolvers/ItemResolver.scala
+++ app/resolvers/ItemResolver.scala
@@ -1,14 +1,18 @@
 package resolvers
 
+import com.google.inject.Inject
 import models.Item
-import models.Item.GraphQL.ItemInput
+import models.Item.GraphQL.{ItemInput, ItemsPayload}
+import repositories.ItemRepository
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class ItemResolver {
+class ItemResolver @Inject()(itemRepository: ItemRepository,
+                             implicit val executionContext: ExecutionContext) {
 
-  def items: Future[List[Item]] = ???
+  def items: Future[ItemsPayload] = for {
+    result <- itemRepository.findAll()
+  } yield ItemsPayload(result)
 
-  def addItem (input: ItemInput): Future[Item] = ???
-
+  def addItem (input: ItemInput): Future[Item] = itemRepository.create(Item(1,input.description))
 }
