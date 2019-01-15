ef7cee8c38a49b4d7b9307e16f0e7156c3f37aef
pavel.grudina <plgrudina@gmail.com>
2019-01-15 17:56:18 +0200

    Added filling with test data

--- test/specs/TestHelper.scala
+++ test/specs/TestHelper.scala
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit
 import akka.stream.Materializer
 import akka.util.Timeout
 import controllers.AppController
+import graphql.resolvers.PostResolver
+import org.scalatest.BeforeAndAfter
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.test.Injecting
@@ -13,12 +15,19 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
 
-trait TestHelper extends PlaySpec with GuiceOneAppPerSuite with Injecting with PreparedInput{
+trait TestHelper extends PlaySpec with GuiceOneAppPerSuite with Injecting with BeforeAndAfter with PreparedInput{
 
   implicit lazy val executionContext: ExecutionContext = inject[ExecutionContext]
+  implicit lazy val postResolver: PostResolver = inject[PostResolver]
   implicit lazy val materializer: Materializer = app.materializer
   implicit lazy val timeout: Timeout = Timeout(20, TimeUnit.SECONDS)
   lazy val appController: AppController = inject[AppController]
+
+  before {
+    postResolver.addPost(title = "First post", content = "Some content")
+    postResolver.addPost(title = "Second post", content = "Some content")
+    postResolver.addPost(title = "Third post", content = "Some content")
+  }
 
   def await[T](asyncFunc: => Future[T]): T = Await.result[T](asyncFunc, Duration.Inf)
 
