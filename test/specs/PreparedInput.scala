4f4afb6ba85bcae683322bc8aaf3da1f84aeed03
pavel.grudina <plgrudina@gmail.com>
2019-01-15 17:57:20 +0200

    Fixed input raw format

--- test/specs/PreparedInput.scala
+++ test/specs/PreparedInput.scala
@@ -1,13 +1,13 @@
 package specs
 
+import play.api.libs.json.{JsValue, Json}
+
 trait PreparedInput {
 
-  lazy val addPost = "{ \"mutation\": \"{ addPost( \"title\":\"Some Post\" \"content\": \"First post\"){ id title content } }\" }"
-  lazy val updatePost = "{ \"mutation\": \"{ updatePost( \"id\":2 \"title\": \"new title\" \"content\": \"X#X#X#X#X#\"){ id title content } }\" }"
-  lazy val findPost = "{ \"query\": \"{ findPost( \"id\":1 ){ id title content} }\" }"
-  lazy val deletePost = "{ \"mutation\": \"{ deletePost( \"id\": 3 ) }\" }"
-  lazy val posts = "{ \"query\": \"{ posts { id title content } }\"}"
+  lazy val addPost: JsValue = Json.parse("{ \"query\": \"mutation { addPost(title:\"Some Post\" content:\"First post\") { id title content } }\" }")
+  lazy val updatePost: JsValue = Json.parse("{ \"query\": \"mutation { updatePost( id: 2 title: \"New title\" content: \"X#X#X#X#X#\"){ id title content } }\" }")
+  lazy val deletePost: JsValue = Json.parse("{ \"query\": \"mutation { deletePost( id: 1 ) }\" }")
 
-
-
+  lazy val findPost: JsValue = Json.parse("{ \"query\": \"query { findPost(id:1) { id title content} }\" }")
+  lazy val posts: JsValue = Json.parse("{ \"query\": \"query { posts { id title content } }\"}")
 }
