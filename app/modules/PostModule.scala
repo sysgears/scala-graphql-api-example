7afa876cb4ce60ab92068c27beea92ad2eb857ca
pavel.grudina <plgrudina@gmail.com>
2019-02-12 15:27:26 +0200

    Formatted naming

--- app/modules/PostModule.scala
+++ app/modules/PostModule.scala
@@ -3,7 +3,7 @@ package modules
 import com.google.inject._
 import repositories.{PostRepositoryImpl, PostRepository}
 
-class PostBindings extends AbstractModule {
+class PostModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[PostRepository]).to(classOf[PostRepositoryImpl]).in(Scopes.SINGLETON)
   }
