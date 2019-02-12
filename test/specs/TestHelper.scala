2e99396ce8ff168f8d44c9a0476b140c579a1eff
pavel.grudina <plgrudina@gmail.com>
2019-02-12 17:16:41 +0200

    Removed unused components

--- test/specs/TestHelper.scala
+++ test/specs/TestHelper.scala
@@ -5,15 +5,11 @@ import java.util.concurrent.TimeUnit
 import akka.stream.Materializer
 import akka.util.Timeout
 import controllers.AppController
-
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.test.Injecting
 
-import repositories.PostRepositoryImpl
-
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 /**
   * Prepares tools for easy writing and execution of tests.
@@ -37,21 +33,7 @@ trait TestHelper extends PlaySpec with GuiceOneAppPerSuite with Injecting with P
   implicit lazy val timeout: Timeout = Timeout(20, TimeUnit.SECONDS)
 
   /**
-    * Injects instance of PostRepository.
-    */
-  lazy val postRepo: PostRepositoryImpl = inject[PostRepositoryImpl]
-
-  /**
     * Injects instance of AppController.
     */
   lazy val appController: AppController = inject[AppController]
-
-  /**
-    * Awaits for the given async function to complete and returns its result
-    *
-    * @param asyncFunc an async func to await for
-    * @tparam T a return type of a given async func
-    * @return a return value of a given async func
-    */
-  def await[T](asyncFunc: => Future[T]): T = Await.result[T](asyncFunc, Duration.Inf)
 }
