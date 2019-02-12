ac09d9c3a2261f46618bf53f4253f1924a729cef
pavel.grudina <plgrudina@gmail.com>
2019-02-12 15:22:40 +0200

    Added extends from 'UserFacingError' for errors

--- app/errors/AlreadyExists.scala
+++ app/errors/AlreadyExists.scala
@@ -1,3 +1,12 @@
 package errors
 
-case class AlreadyExists(msg: String = "") extends Error(msg)
+import sangria.execution.UserFacingError
+
+/**
+  * Represents an exception object indicating that something is already exists.
+  *
+  * @param msg an exception message to show
+  */
+case class AlreadyExists(msg: String) extends Exception with UserFacingError {
+  override def getMessage: String = msg
+}
--- app/errors/NotFound.scala
+++ app/errors/NotFound.scala
@@ -1,3 +1,13 @@
 package errors
 
-case class NotFound(msg: String = "") extends Error(msg)
+import sangria.execution.UserFacingError
+
+/**
+  * Represents an exception object indicating that something was not found.
+  *
+  * @param msg an exception message to show
+  */
+case class NotFound(msg: String) extends Exception(msg) with UserFacingError {
+  override def getMessage(): String = msg
+}
+
